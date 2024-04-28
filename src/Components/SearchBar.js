996b7b95b0a327b04f94e85ba481ac084f08f7d6
Arnold Kisuri <arnoldkisuri7@gmail.com>
2024-04-29 02:24:58 +0300

    add styling

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,12 +1,15 @@
-function SearchBar(){
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+function SearchBar({onSearchChange}){
     return (
         <div className="searchbar">
           <input
             type="text"
             placeholder="Search your Recent Transactions"
-            // onChange={(e) =>handleSearch(e.target.value)}
+            onChange={(e) =>onSearchChange(e.target.value)}
           />
-          <i className="circular search link icon"></i>
+          <FontAwesomeIcon icon={faSearch} className="search-icon" />
         </div>
       );
 }
