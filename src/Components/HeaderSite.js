c6b381cbaa1e53a85276918d6445af92d7c3e846
Arnold Kisuri <arnoldkisuri7@gmail.com>
2024-04-28 22:38:05 +0300

    Add Site Header

---
+++ src/Components/HeaderSite.js
@@ -0,0 +1,10 @@
+function HeaderSite(){
+    return(
+        <div className="site-header">
+            <title>Flatiron Bank</title>
+            <h1>The Royal Bank of Flatiron</h1>
+        </div>
+        
+    )
+}
+export default HeaderSite
