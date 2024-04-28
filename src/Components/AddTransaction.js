ee0409266875cae86ff1ed41635aa5cbdd4b453a
Arnold Kisuri <arnoldkisuri7@gmail.com>
2024-04-29 00:21:35 +0300

    Add useState

--- src/Components/AddTransaction.js
+++ src/Components/AddTransaction.js
@@ -1,3 +1,5 @@
+import React, {useState} from "react"
+
 function AddTransaction(){
     return(
         <div className="add-transaction">
--- src/Components/FilterContainer.js
+++ src/Components/FilterContainer.js
@@ -2,13 +2,28 @@ import SearchBar from "./SearchBar";
 import AddTransaction from "./AddTransaction";
 import Transactions from "./Transaction";
 import TransactionTable from "./TransactionTable";
+import React, {useState} from "react";
 
 function FilterContainer(){
+    const [transactions, setTransactions] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleAddTransaction = (newTransaction) => {
+      setTransactions([...transactions, newTransaction]);
+    };
+
+    const handleSearchChange = (term) => {
+      setSearchTerm(term);
+    };
+
+    const filteredTransactions = transactions.filter((transaction) =>
+      transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+    );
     return(
         <div>
-            <SearchBar/>
-            <AddTransaction/>
-            <TransactionTable/>
+            <SearchBar onSearchChange={handleSearchChange}/>
+            <AddTransaction onAddTransaction={handleAddTransaction}/>
+            <TransactionTable transactionsFun = {filteredTransactions}/>
             
         </div>
     )
