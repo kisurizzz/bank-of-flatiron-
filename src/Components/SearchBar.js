08ece67db029d3b8bb3d45278618a083a07e4d0a
Arnold Kisuri <arnoldkisuri7@gmail.com>
2024-04-29 03:10:03 +0300

    More Styling

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -3,14 +3,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 function SearchBar({onSearchChange}){
     return (
-        <div className="searchbar">
-          <input
-            type="text"
-            placeholder="Search your Recent Transactions"
-            onChange={(e) =>onSearchChange(e.target.value)}
-          />
-          <FontAwesomeIcon icon={faSearch} className="search-icon" />
-        </div>
-      );
+        <div className="search-container"> {/* Optional class for overall styling */}
+      <label >Search Transactions:</label>
+      <div className="search-input">  {/* Add a class for styling the input field */}
+        <FontAwesomeIcon icon={faSearch} className="search-icon" />
+        <input
+          type="text"
+          id="search"
+          placeholder="Search by description"
+          onChange={(e) => onSearchChange(e.target.value)}
+        />
+      </div>
+    </div>
+  );
 }
 export default SearchBar
